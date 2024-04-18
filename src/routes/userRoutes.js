e035b6d02149f243f9f3adc0fd3f90eba440cde4
weiner-rogerio <weinerrogerio.com>
2024-04-17 21:02:01 -0300

    add jwt users 1/2

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,17 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const router = new Router();
 
+// isso não deveria existir em um sistema real --> retirar se usar para aplicação real
+router.get('/', loginRequired, UserController.index);// lista TODOS os usuários
+router.get('/:id', UserController.show);// lista UM usuário (qualquer um)
+
+// realmente necessários:
 router.post('/', UserController.store);
-router.get('/', UserController.index);
-router.get('/:id', UserController.show);
-router.put('/:id', UserController.update);
-router.delete('/:id', UserController.delete);
+router.put('/', loginRequired, UserController.update);
+router.delete('/', loginRequired, UserController.delete);
 
 export default router;
